7f8e938fb814f4a8e70961f31dee9a86a2659488
Anton Tokarev <anton999@bk.ru>
2023-04-14 11:26:26 +0500

    Done!

--- bot.js
+++ bot.js
@@ -6,6 +6,8 @@ const TOKEN = "6091844559:AAG2p68kB5546x1TFt17l5Kfpb1-haQ3x3M";
 const bot = new Telegraf(TOKEN);
 
 const data = JSON.parse(fs.readFileSync("date.json"));
+console.log(data.facts);
+let timeDrink = 0;
 let time;
 let drink = false;
 let emoji = ["😁", "😄", "😅", "😠", "😡", "🥵", "🥶", "🤡", "💀"];
@@ -22,23 +24,32 @@ let emojiSuccess = [
   "🎺",
   "🎷",
 ];
-const awordEmoji = ['💚','💛','🧡','❤️','❤️‍🔥'];
+const awardEmojiList = ["💚", "💛", "🧡", "❤️", "❤️‍🔥"];
+const awadEmojiAll = [];
 
 const mainKeybord = Markup.keyboard(
   [
     "😍 Напомнить попить водички!",
-    "✅ Выпил!",
-    "❌ больше не напоминать!",
     "Времени осталось",
+    "✅ Выпил!",
+    "❌ Больше не напоминать!",
+    "👑 Награды",
   ],
   {
-    wrap: (btn, index, currentRow) => currentRow.length >= (index + 1) / 2,
+    wrap: (btn, index, currentRow) => currentRow.length >= (index + 1) / 3,
   }
 );
 
+const awardKeybord = Markup.keyboard([], {
+  wrap: (btn, index, currentRow) => currentRow.length >= (index + 1) / btn,
+}).resize();
+
 bot.start(async (ctx) => {
-   await ctx.reply(`Привет! ${ctx.chat.first_name} 👑`, mainKeybord.oneTime().resize());
-   console.log(ctx.message)
+  await ctx.reply(
+    `Привет! ${ctx.chat.first_name} 👑`,
+    mainKeybord.oneTime().resize()
+  );
+  console.log(ctx.message);
 });
 
 function timeWater(ctx) {
@@ -81,6 +92,34 @@ bot.hears("😍 Напомнить попить водички!", async (ctx, ne
 });
 
 bot.hears("✅ Выпил!", async (ctx) => {
+  timeDrink++;
+  switch (timeDrink) {
+    case 10:
+      awadEmojiAll.push(awardEmojiList[0]);
+      awardKeybord.reply_markup.keyboard.push(awadEmojiAll);
+      await ctx.reply("💚 загляни в награды!");
+      break;
+    case 20:
+      awadEmojiAll.push(awardEmojiList[1]);
+      awardKeybord.reply_markup.keyboard.splice(0, 1, awadEmojiAll);
+      await ctx.reply("💛 загляни в награды!");
+      break;
+    case 30:
+      awadEmojiAll.push(awardEmojiList[2]);
+      awardKeybord.reply_markup.keyboard.splice(0, 1, awadEmojiAll);
+      await ctx.reply("🧡 загляни в награды!");
+      break;
+    case 40:
+      awadEmojiAll.push(awardEmojiList[3]);
+      awardKeybord.reply_markup.keyboard.splice(0, 1, awadEmojiAll);
+      await ctx.reply("❤️ загляни в награды!");
+      break;
+    case 50:
+      awadEmojiAll.push(awardEmojiList[4]);
+      awardKeybord.reply_markup.keyboard.splice(0, 1, awadEmojiAll);
+      await ctx.reply("❤️‍🔥 загляни в награды!");
+      break;
+  }
   drink = false;
   setTimeout(() => {
     drink = true;
@@ -89,13 +128,13 @@ bot.hears("✅ Выпил!", async (ctx) => {
 
   const randomEmoji =
     emojiSuccess[Math.floor(Math.random() * emojiSuccess.length)];
-  const randomFact = data[Math.floor(Math.random() * data.length)];
+  const randomFact = data.facts[Math.floor(Math.random() * data.facts.length)];
 
   await ctx.reply(`${randomEmoji} следующий прием через 2 часа`);
   await ctx.reply(randomFact);
 });
 
-bot.hears("❌ больше не напоминать!", async (ctx) => {
+bot.hears("❌ Больше не напоминать!", async (ctx) => {
   drink = false;
   ctx.reply("Хорошо!");
 });
@@ -113,5 +152,32 @@ bot.hears("Времени осталось", (ctx) => {
   }
 });
 
-console.log('Start!!!')
+bot.hears("👑 Награды", async (ctx) => {
+  timeDrink < 2
+    ? ctx.reply("У тебя ещё нет наград :(")
+    : ctx.reply("Награды 📈", awardKeybord);
+});
+
+bot.hears("💚", async (ctx) => {
+  await ctx.replyWithPhoto({ url: data.image["10"].src });
+  await ctx.reply(data.image["10"].text, mainKeybord);
+});
+bot.hears("💛", async (ctx) => {
+  await ctx.replyWithPhoto({ url: data.image["20"].src });
+  await ctx.reply(data.image["20"].text, mainKeybord);
+});
+bot.hears("🧡", async (ctx) => {
+  await ctx.replyWithPhoto({ url: data.image["30"].src });
+  await ctx.reply(data.image["30"].text, mainKeybord);
+});
+bot.hears("❤️", async (ctx) => {
+  await ctx.replyWithPhoto({ url: data.image["40"].src });
+  await ctx.reply(data.image["40"].text, mainKeybord);
+});
+bot.hears("❤️‍🔥", async (ctx) => {
+  await ctx.replyWithPhoto({ url: data.image["50"].src });
+  await ctx.reply(data.image["50"].text, mainKeybord);
+});
+
+console.log("Start!!!");
 bot.launch();
